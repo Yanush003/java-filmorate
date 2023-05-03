25100d75d00edf67a767edf8ab6e6a304e13d335
Mariya <pavlov303@yandex.ru>
2023-05-03 13:16:22 +0300

    поменять аннотацию

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import ru.yandex.practicum.filmorate.annotation.ValidDate;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Positive;
 import javax.validation.constraints.Size;
 import java.time.LocalDate;
