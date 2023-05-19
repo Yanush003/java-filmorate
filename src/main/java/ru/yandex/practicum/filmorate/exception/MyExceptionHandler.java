cdf7c10215fe56d2e374934638695877b3e27535
Mariya <pavlov303@yandex.ru>
2023-05-19 20:47:12 +0300

    тз10

--- src/main/java/ru/yandex/practicum/filmorate/exception/MyExceptionHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/MyExceptionHandler.java
@@ -3,11 +3,14 @@ package ru.yandex.practicum.filmorate.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.validation.ValidationException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 @ControllerAdvice
