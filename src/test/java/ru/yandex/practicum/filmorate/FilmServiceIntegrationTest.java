7c6e0574a71401cdb538c3c9fe385f5f79626ebe
Maria <pavlov303@yandex.ru>
2023-06-12 23:06:20 +0300

    тз11

--- src/test/java/ru/yandex/practicum/filmorate/FilmServiceIntegrationTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmServiceIntegrationTest.java
@@ -5,31 +5,41 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.service.UserService;
+import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
 
 import java.time.LocalDate;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class FilmServiceIntegrationTest {
 
-    private final FilmService filmService;
+    @Autowired
+    private FilmService filmService;
+
+    @Autowired
+    private FilmDbStorage filmDbStorage;
+
+    @Autowired
+    private UserService userService;
 
     @Test
     public void testGetFilmById() {
-        // Создаем фильм
+        // Создаем фильм в базе данных
         Film film = new Film();
         film.setName("Test Film");
         film.setDescription("Test Description");
-        film.setReleaseDate(LocalDate.of(2021, 1, 1));
+        film.setReleaseDate(LocalDate.now());
         film.setDuration(120);
-        Film film1 = filmService.saveFilm(film);
+        Film film1 = filmDbStorage.create(film);
 
         // Получаем фильм по ID
         Film result = filmService.getFilmById(film1.getId());
@@ -39,146 +49,140 @@ public class FilmServiceIntegrationTest {
     }
 
     @Test
-    public void testGetFilmByIdWithNullId() {
-        // Пытаемся получить фильм с null ID
-        Throwable exception = assertThrows(IllegalArgumentException.class, () -> filmService.getFilmById(null));
-
-        // Проверяем, что было выброшено исключение IllegalArgumentException
-        assertEquals("ID must not be null", exception.getMessage());
-    }
-
-    @Test
     public void testPutLike() {
-        // Создаем фильм
+        // Создаем фильм в базе данных
         Film film = new Film();
         film.setName("Test Film");
         film.setDescription("Test Description");
-        film.setReleaseDate(LocalDate.of(2021, 1, 1));
+        film.setReleaseDate(LocalDate.now());
         film.setDuration(120);
-        Film film1 = filmService.saveFilm(film);
+        Film film1 = filmDbStorage.create(film);
 
-        // Добавляем лайк
-        Long userId = 1L;
-        filmService.putLike(film1.getId(), userId);
+        // Создаем пользователя в базе данных
+        User user = new User();
+        user.setName("Test User");
+        user.setLogin("testuser");
+        user.setEmail("testuser@example.com");
+        user.setBirthday(LocalDate.now());
+        User user1 = userService.saveUser(user);
 
-        // Проверяем, что лайк был добавлен
-        Film result = filmService.getFilmById(film1.getId());
-        assertTrue(result.getUserIds().contains(userId));
+        // Ставим лайк на фильм от пользователя
+        filmService.putLike(film1.getId(), user1.getId());
     }
 
     @Test
     public void testDeleteLike() {
-        // Создаем фильм
+        // Создаем фильм в базе данных
         Film film = new Film();
         film.setName("Test Film");
         film.setDescription("Test Description");
-        film.setReleaseDate(LocalDate.of(2021, 1, 1));
+        film.setReleaseDate(LocalDate.now());
         film.setDuration(120);
-        film = filmService.saveFilm(film);
+        Film film1 = filmDbStorage.create(film);
 
-        // Добавляем лайк
-        Long userId = 1L;
-        filmService.putLike(film.getId(), userId);
+        // Создаем пользователя в базе данных
+        User user = new User();
+        user.setName("Test User");
+        user.setLogin("testuser");
+        user.setEmail("testuser@example.com");
+        user.setBirthday(LocalDate.now());
+        User user1 = userService.saveUser(user);
 
-        // Удаляем лайк
-        filmService.deleteLike(film.getId(), userId);
+        // Ставим лайк на фильм от пользователя
+        filmService.putLike(film1.getId(), user1.getId());
 
-        // Проверяем, что лайк был удален
-        Film result = filmService.getFilmById(film.getId());
-        assertFalse(result.getUserIds().contains(userId));
+        // Удаляем лайк на фильм от пользователя
+        filmService.deleteLike(film1.getId(), user1.getId());
+
+        // Получаем обновленный фильм из базы данных
+        Film updatedFilm = filmDbStorage.get(film1.getId());
+
+        // Проверяем, что пользователь удален из списка лайков фильма
+        assertFalse(updatedFilm.getUserIds().contains(user1.getId()));
     }
 
     @Test
     public void testGetFilms() {
-        // Создаем несколько фильмов
+        // Создаем несколько фильмов в базе данных
         Film film1 = new Film();
-        film1.setName("Film 1");
-        film1.setDescription("Description 1");
-        film1.setReleaseDate(LocalDate.of(2021, 1, 1));
+        film1.setName("Test Film 1");
+        film1.setDescription("Test Description 1");
+        film1.setReleaseDate(LocalDate.now());
         film1.setDuration(120);
-        Film film1Result = filmService.saveFilm(film1);
+        Film film = filmDbStorage.create(film1);
 
         Film film2 = new Film();
-        film2.setName("Film 2");
-        film2.setDescription("Description 2");
-        film2.setReleaseDate(LocalDate.of(2021, 2, 1));
+        film2.setName("Test Film 2");
+        film2.setDescription("Test Description 2");
+        film2.setReleaseDate(LocalDate.now());
         film2.setDuration(90);
-        Film film2Result = filmService.saveFilm(film2);
+        Film film3 = filmDbStorage.create(film2);
 
-        Film film3 = new Film();
-        film3.setName("Film 3");
-        film3.setDescription("Description 3");
-        film3.setReleaseDate(LocalDate.of(2021, 3, 1));
-        film3.setDuration(150);
-        Film film3Result = filmService.saveFilm(film3);
+        // Создаем несколько пользователей в базе данных
+        User user1 = new User();
+        user1.setName("Test User 1");
+        user1.setLogin("testuser1");
+        user1.setEmail("testuser1@example.com");
+        user1.setBirthday(LocalDate.now());
+        User user = userService.saveUser(user1);
 
-        // Получаем топ-2 фильмов по лайкам
-        List<Film> result = filmService.getFilms(2);
+        User user2 = new User();
+        user2.setName("Test User 2");
+        user2.setLogin("testuser2");
+        user2.setEmail("testuser2@example.com");
+        user2.setBirthday(LocalDate.now());
+        User user3 = userService.saveUser(user2);
 
-        // Проверяем, что полученные фильмы соответствуют ожидаемым
-        assertEquals(2, result.size());
-        assertEquals(film1Result, result.get(0));
-        assertEquals(film2Result, result.get(1));
+        // Ставим лайки на фильмы от пользователей
+        filmService.putLike(film.getId(), user.getId());
+        filmService.putLike(film.getId(), user3.getId());
+        filmService.putLike(film3.getId(), user.getId());
+
+        // Получаем список фильмов, отсортированных по количеству лайков
+        List<Film> films = filmService.getFilms(2);
+
+        // Провер, что полученный список фильмов соответствует ожидаемому
+        assertEquals(2, films.size());
     }
 
     @Test
     public void testSaveFilm() {
-        // Создаем фильм
+        // Создаем фильм для сохранения
         Film film = new Film();
         film.setName("Test Film");
         film.setDescription("Test Description");
-        film.setReleaseDate(LocalDate.of(2021, 1, 1));
+        film.setReleaseDate(LocalDate.now());
         film.setDuration(120);
 
-        // Сохраняем фильм
-        Film result = filmService.saveFilm(film);
+        // Сохраняем фильм в базе данных
+        Film savedFilm = filmService.saveFilm(film);
 
-        // Проверяем, что фильм был сохранен
-        assertNotNull(result.getId());
-        assertEquals(film.getName(), result.getName());
-        assertEquals(film.getDescription(), result.getDescription());
-        assertEquals(film.getReleaseDate(), result.getReleaseDate());
-        assertEquals(film.getDuration(), result.getDuration());
+        // Получаем фильм из базы данных
+        Film result = filmDbStorage.get(savedFilm.getId());
+
+        // Проверяем, что полученный фильм соответствует ожидаемому
+        assertEquals(savedFilm, result);
     }
 
     @Test
     public void testUpdateFilm() {
-        // Создаем фильм
+        // Создаем фильм для обновления
         Film film = new Film();
         film.setName("Test Film");
         film.setDescription("Test Description");
-        film.setReleaseDate(LocalDate.of(2021, 1, 1));
+        film.setReleaseDate(LocalDate.now());
         film.setDuration(120);
-        film = filmService.saveFilm(film);
+        Film film1 = filmDbStorage.create(film);
 
-        // Обновляем фильм
-        film.setName("Updated Film");
-        film.setDescription("Updated Description");
-        film.setReleaseDate(LocalDate.of(2021, 2, 1));
-        film.setDuration(90);
-        Film result = filmService.updateFilm(film);
+        // Обновляем фильм в базе данных
+        film.setName("Updated Test Film");
+        Film updatedFilm = filmService.updateFilm(film1);
 
-        // Проверяем, что фильм был обновлен
-        assertEquals(film.getId(), result.getId());
-        assertEquals(film.getName(), result.getName());
-        assertEquals(film.getDescription(), result.getDescription());
-        assertEquals(film.getReleaseDate(), result.getReleaseDate());
-        assertEquals(film.getDuration(), result.getDuration());
-    }
+        // Получаем обновленный фильм из базы данных
+        Film result = filmDbStorage.get(updatedFilm.getId());
 
-    @Test
-    public void testUpdateFilmWithNonexistentId() {
-        // Создаем фильм
-        Film film = new Film();
-        film.setId(10L);
-        film.setName("Test Film");
-        film.setDescription("Test Description");
-        film.setReleaseDate(LocalDate.of(2021, 1, 1));
-        film.setDuration(120);
-        // Пытаемся обновить несуществующий фильм
-        Throwable exception = assertThrows(NotFoundException.class, () -> filmService.updateFilm(film));
-
-        // Проверяем, что было выброшено исключение NotFoundException
-        assertEquals("Film not found with ID: " + film.getId(), exception.getMessage());
+        // Проверяем, что полученный фильм соответствует ожидаемому
+        assertEquals(film1, result);
     }
 }
+
